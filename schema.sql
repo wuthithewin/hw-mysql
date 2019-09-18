c816e7ef0fdc5a98f4222553e6770f93c7c07866
wuthithewin <wuthithewin@gmail.comgit config --global user.email wuthithewin@gmail.comgit config --global user.name>
2019-09-17 19:00:43 -0700

    changing the items and prices

--- schema.sql
+++ schema.sql
@@ -6,13 +6,13 @@ USE bamazon;
 CREATE TABLE products(
   item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NOT NULL,
-  department_name VARCHAR(45) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
   price INT default 0,
   stock_quantity INT default 0,
   PRIMARY KEY (item_id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Test 1", "Video Games", 49.95, 150),
-  ("Test 2", "Video Games", 59.99, 200),
+VALUES ("Movie 1", "Game Of Thrones", 10, 150),
+  ("Movie 2", "Walking Dead", 5, 200),
 
